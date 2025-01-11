c83df949dba7120ecd61146b0e6512c65ac2016d
PKFabricate <151477741+PKFabricate@users.noreply.github.com>
2025-01-11 18:56:03 +0100

    Updated Auhtors for 3 (not e yet)

--- source/authors_lab2.h
+++ source/authors_lab2.h
@@ -6,11 +6,11 @@
 #define AUTHORS_TWO_D "Sam Diehl"
 
 #define AUTHORS_THREE_B "Paul Klein"
-#define AUTHORS_THREE_C ""
-#define AUTHORS_THREE_D ""
+#define AUTHORS_THREE_C "Paul Klein"
+#define AUTHORS_THREE_D "Paul Klein"
 #define AUTHORS_THREE_E ""
-#define AUTHORS_THREE_F ""
-#define AUTHORS_THREE_G ""
+#define AUTHORS_THREE_F "Paul Klein"
+#define AUTHORS_THREE_G "Paul Klein"
 
 #define AUTHORS_FOUR_A ""
 #define AUTHORS_FOUR_B ""
