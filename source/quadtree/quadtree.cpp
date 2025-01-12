bcabda14dc702789614ef85f53deba00d6d53559
PKFabricate <151477741+PKFabricate@users.noreply.github.com>
2025-01-12 01:56:06 +0100

    3d fixed? Doesn't seem any faster, could be my computer though.

--- source/quadtree/quadtree.cpp
+++ source/quadtree/quadtree.cpp
@@ -73,44 +73,50 @@ std::vector<QuadtreeNode*> Quadtree::construct(Universe& universe, BoundingBox B
 
 std::vector<QuadtreeNode*> Quadtree::construct_task(Universe& universe, BoundingBox BB, std::vector<std::int32_t> body_indices){
     std::vector<QuadtreeNode*> resulting_children = {};
-
-    
-    for (int i = 0; i < 4; i++) {
-        #pragma omp task shared(universe) default(private)
+#pragma omp parallel
+    {
+    #pragma omp single
         {
-        BoundingBox sub_BB = BB.get_quadrant(i);
-        std::vector<std::int32_t> sub_body_indices = {};
-        //Create subset of body indices that are in that quadrant for further use
-        for (int j = 0; j < body_indices.size(); j++) {
-            int32_t current_indice = body_indices[j];
-            if (sub_BB.contains(universe.positions[current_indice])) {
-                sub_body_indices.push_back(current_indice);
-            }           
+
+    for (int i = 0; i < 4; i++) {
+        #pragma omp task shared(universe, BB, body_indices)
+        {
+            BoundingBox sub_BB = BB.get_quadrant(i);
+            std::vector<std::int32_t> sub_body_indices = {};
+            //Create subset of body indices that are in that quadrant for further use
+            for (int j = 0; j < body_indices.size(); j++) {
+                int32_t current_indice = body_indices[j];
+                if (sub_BB.contains(universe.positions[current_indice])) {
+                    sub_body_indices.push_back(current_indice);
+                }
+            }
+            //If quadrant isn't empty, create a Quadtree node with that Quadtrant, then recursively use condtruct to create that node's children. If it's empty, ignore quadrant.
+            if (!sub_body_indices.empty()) {
+                QuadtreeNode* new_child = new QuadtreeNode(sub_BB);
+               
+                //If only one body in child (thus a leaf node), update body identifier, center of mass and cumulative mass, else recursive construction.
+                //Cumulative Mass and Center of Mass are the mass/position of the sole body
+                if (sub_body_indices.size() == 1) {
+                    new_child->body_identifier = sub_body_indices[0];
+                    new_child->cumulative_mass = universe.weights[sub_body_indices[0]];
+                    new_child->cumulative_mass_ready = true;
+                    new_child->center_of_mass = universe.positions[sub_body_indices[0]];
+                    new_child->center_of_mass_ready = true;
+
+                }
+
+                else {
+                    new_child->children = construct_task(universe, sub_BB, sub_body_indices);
+
+                }
+                resulting_children.push_back(new_child);
+            }
         }
-        //If quadrant isn't empty, create a Quadtree node with that Quadtrant, then recursively use condtruct to create that node's children. If it's empty, ignore quadrant.
-        if (!sub_body_indices.empty()) {
-            QuadtreeNode* new_child = new QuadtreeNode(sub_BB);
-            resulting_children.push_back(new_child);
-            //If only one body in child (thus a leaf node), update body identifier, center of mass and cumulative mass, else recursive construction.
-            //Cumulative Mass and Center of Mass are the mass/position of the sole body
-            if (sub_body_indices.size() == 1) {
-                new_child->body_identifier = sub_body_indices[0];
-                new_child->cumulative_mass = universe.weights[sub_body_indices[0]];
-                new_child->cumulative_mass_ready = true;
-                new_child->center_of_mass = universe.positions[sub_body_indices[0]];
-                new_child->center_of_mass_ready = true;
-
-            }
-
-            else {
-                new_child->children = construct(universe, sub_BB, sub_body_indices);
-                
-            }
+        }
         }
     }
           //Return the children of the Node that the Node corresponding to the current Bounding Box is supposed to get.
     return resulting_children;
-    }
 }
 
 std::vector<QuadtreeNode*> Quadtree::construct_task_with_cutoff(Universe& universe, BoundingBox& BB, std::vector<std::int32_t>& body_indices){
